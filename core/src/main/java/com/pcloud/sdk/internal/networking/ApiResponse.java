d6e35a2146d5815048e48364c84ef09049f90992
Georgi Neykov <georgi.neykov@pcloud.com>
2017-01-10 14:55:23 +0200

    feat (Core): Add basic response types

---
+++ core/src/main/java/com/pcloud/sdk/internal/networking/ApiResponse.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2017 pCloud AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pcloud.sdk.internal.networking;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public abstract class ApiResponse {
+
+    @Expose
+    @SerializedName("error")
+    private String message;
+
+    @Expose
+    @SerializedName("result")
+    private int statusCode;
+
+    protected ApiResponse() {
+    }
+
+    public ApiResponse(int statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public boolean isSuccessful() {
+        return statusCode == Status.SUCCESS;
+    }
+}

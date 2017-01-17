0a704c64c8fd28a91d2b0bda858ee4e7e399a355
Gergana Vasileva <gergana.vasileva@pcloud.com>
2017-01-17 14:54:14 +0200

    refactor(Android): Convert android module to library.

--- android/src/main/java/com/pcloud/authentication/AuthenticationActivity.java
+++ android/src/main/java/com/pcloud/authentication/AuthenticationActivity.java
@@ -5,22 +5,21 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.pcloud.authentication;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -33,7 +32,7 @@ import static com.pcloud.authentication.Constants.API_CALL_URI;
 import static com.pcloud.authentication.Constants.API_KEY;
 import static com.pcloud.authentication.Constants.REDIRECT_URI;
 
-public class AuthenticationActivity extends AppCompatActivity {
+public class AuthenticationActivity extends Activity {
 
     private static final String TAG = AuthenticationActivity.class.getSimpleName();
 

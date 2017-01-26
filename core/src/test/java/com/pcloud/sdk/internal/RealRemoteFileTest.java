e9709f71ac1d8c313c4c781f84fa38f8a56c3a6f
Georgi Neykov <georgi.neykov@pcloud.com>
2017-01-26 17:21:57 +0200

    test(Core): Add a basic test for RealRemoteFile

---
+++ core/src/test/java/com/pcloud/sdk/internal/RealRemoteFileTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2017 pCloud AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.pcloud.sdk.internal;
+
+import com.pcloud.sdk.api.ApiService;
+import com.pcloud.sdk.api.BaseRemoteDataTest;
+import com.pcloud.sdk.utils.DummyDownloadingApiService;
+import org.junit.Before;
+
+import static org.mockito.Mockito.*;
+
+public class RealRemoteFileTest extends BaseRemoteDataTest {
+
+    private RealRemoteFile testInstance;
+    private ApiService apiService;
+
+    @Before
+    public void setUp() throws Exception {
+        apiService = spy(DummyDownloadingApiService.create());
+        testInstance = new RealRemoteFile(apiService);
+        testInstance(testInstance);
+    }
+}
